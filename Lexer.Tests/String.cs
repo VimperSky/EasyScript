6098f3cbee87313246edb56fae83456e626e65bd
Roman <carapacik@gmail.com>
2021-02-17 18:51:18 +0300

    Добавил пробных тестов

---
+++ Lexer.Tests/String.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Xunit;
+using Lexer.States;
+
+namespace Lexer.Tests
+{
+    public class String
+    {
+        [Fact]
+        public void DefaultString()
+        {
+            const string str = "\"some string\"";
+            var machine = new LexerMachine();
+            foreach (var t in str)
+            {
+                machine.ProcessChar(t, 0, 0);
+            }
+
+            Assert.Equal(machine.GetTokens().First().Type, TokenType.String);
+            
+        }
+    }
+}

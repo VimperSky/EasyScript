d886d782d36f03eeb27c97120596b2df0826b9b7
Roman <carapacik@gmail.com>
2021-04-22 17:39:04 +0300

    Test from Pozirifkina

--- LLGenerator.Tests/SetsParserTests.cs
+++ LLGenerator.Tests/SetsParserTests.cs
@@ -43,5 +43,25 @@ namespace LLGenerator.Tests
             const string expected = "F -> S1 $ / f, a\r\nS1 -> f C / f\r\nC -> S1 / f, a\r\nC -> A / c\r\nC -> a / a\r\nS1 -> a D / a\r\nD -> A / c\r\nD -> d / d\r\nA -> c B / c\r\nB -> a B / a\r\nB -> b B / b\r\nB -> e / $\r\n";
             Assert.Equal(expected, sw.ToString());
         }
+
+        // F -> S $ / x
+        // S -> x B / x
+        // B -> A B / y
+        // B -> e / $
+        // A -> y C / y
+        // C -> S C / x
+        // C -> e / y, $
+        [Fact]
+        public void FckingTest4()
+        {
+            var rulesStream = File.OpenRead("../../../test4.txt");
+            var dirRules = SetsParser.SetsParser.DoParse(rulesStream);
+            var sw = new StringWriter();
+            foreach (var rule in dirRules)
+                sw.WriteLine(rule);
+            const string expected = "F -> S $ / x\r\nS -> x B / x\r\nB -> A B / y\r\nB -> e / $\r\nA -> y C / y\r\nC -> S C / x\r\nC -> e / y, $\r\n";
+            Assert.Equal(expected, sw.ToString());
+        }
+       
     }
 }

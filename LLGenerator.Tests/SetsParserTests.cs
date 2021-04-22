e689efffcc1fc2bac0422ac327bf06047dbd4c98
Roman <carapacik@gmail.com>
2021-04-22 21:48:01 +0300

    Ошибка?

--- LLGenerator.Tests/SetsParserTests.cs
+++ LLGenerator.Tests/SetsParserTests.cs
@@ -43,14 +43,7 @@ namespace LLGenerator.Tests
             const string expected = "F -> S1 $ / f, a\r\nS1 -> f C / f\r\nC -> S1 / f, a\r\nC -> A / c\r\nC -> a / a\r\nS1 -> a D / a\r\nD -> A / c\r\nD -> d / d\r\nA -> c B / c\r\nB -> a B / a\r\nB -> b B / b\r\nB -> e / $\r\n";
             Assert.Equal(expected, sw.ToString());
         }
-
-        // F -> S $ / x
-        // S -> x B / x
-        // B -> A B / y
-        // B -> e / $
-        // A -> y C / y
-        // C -> S C / x
-        // C -> e / y, $
+        
         [Fact]
         public void FckingTest4()
         {
@@ -62,6 +55,17 @@ namespace LLGenerator.Tests
             const string expected = "F -> S $ / x\r\nS -> x B / x\r\nB -> A B / y\r\nB -> e / $, x, y\r\nA -> y C / y\r\nC -> S C / x\r\nC -> e / y, $, x\r\n";
             Assert.Equal(expected, sw.ToString());
         }
-       
+
+        [Fact]
+        public void FckingTest5()
+        {
+            var rulesStream = File.OpenRead("../../../test5.txt");
+            var dirRules = SetsParser.SetsParser.DoParse(rulesStream);
+            var sw = new StringWriter();
+            foreach (var rule in dirRules)
+                sw.WriteLine(rule);
+            const string expected = "F -> S $ / (, b\r\nS -> ( S ) A / (\r\nS -> b A / b\r\nA -> a A / a\r\nA -> e / $\r\n";
+            Assert.Equal(expected, sw.ToString());
+        }
     }
 }
